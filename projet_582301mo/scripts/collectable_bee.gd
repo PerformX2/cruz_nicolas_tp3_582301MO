b31ffb159b01cf2e920ea87ea649e9ee4dccbb43
Nicolas Cruz <143363153+PerformX2@users.noreply.github.com>
2025-11-07 22:53:20 -0500

    Configuration zones et debut programmation (bees, honey, portail)

--- projet_582301mo/scripts/collectable_bee.gd
+++ projet_582301mo/scripts/collectable_bee.gd
@@ -1,19 +1,20 @@
-extends Area2D
+extends Node
 
-var is_collected = false
-var stream_paused = true
+@export var collectable_bees = 0
+signal collectable_bee_collected
 
-func _on_body_entered(body):
-	if body is PlayerController and not is_collected:
-		is_collected = true
-		stream_paused = false
-		$sfx_collected_bee.play()
-		GameManager.add_collectable_bee()
+var objectif_collectable_bees_level_2 = 4
 
-		# Create a tween to scale down smoothly
-		var tween = create_tween()
-		tween.tween_property(self, "scale", Vector2(0, 0), 0.3).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BACK)
+signal honeybucket_collected
 
-		# Wait for the tween to finish, then delete the node
-		await get_tree().create_timer(1.8).timeout
-		queue_free()
+func unlock_portal(portal):
+	honeybucket_collected.emit(portal)
+
+
+func add_collectable_bees():
+	collectable_bees += 1
+	collectable_bee_collected.emit(collectable_bees)
+
+func change_scene(new_scene:PackedScene):
+	print("change scene")
+	get_tree().call_deferred("change_scene_to_packed", new_scene)
